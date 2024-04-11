cc8d9a777bc26337d5766acffd93cd8325aa6dd5
ajai <ajaikumarvs@gmail.com>
2024-04-11 18:31:54 +0530

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace ExtBloq
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.nametextBox = new System.Windows.Forms.TextBox();
+            this.addresstextBox = new System.Windows.Forms.TextBox();
+            this.phonetextBox = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 46);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(159, 48);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 100);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(491, 703);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(657, 31);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(130, 49);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Student Record";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(509, 100);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(540, 566);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // nametextBox
+            // 
+            this.nametextBox.Location = new System.Drawing.Point(1064, 111);
+            this.nametextBox.Name = "nametextBox";
+            this.nametextBox.Size = new System.Drawing.Size(351, 22);
+            this.nametextBox.TabIndex = 5;
+            // 
+            // addresstextBox
+            // 
+            this.addresstextBox.Location = new System.Drawing.Point(1064, 139);
+            this.addresstextBox.Name = "addresstextBox";
+            this.addresstextBox.Size = new System.Drawing.Size(351, 22);
+            this.addresstextBox.TabIndex = 6;
+            this.addresstextBox.TextChanged += new System.EventHandler(this.addresstextBox_TextChanged);
+            // 
+            // phonetextBox
+            // 
+            this.phonetextBox.Location = new System.Drawing.Point(1123, 167);
+            this.phonetextBox.Name = "phonetextBox";
+            this.phonetextBox.Size = new System.Drawing.Size(100, 22);
+            this.phonetextBox.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1440, 802);
+            this.Controls.Add(this.phonetextBox);
+            this.Controls.Add(this.addresstextBox);
+            this.Controls.Add(this.nametextBox);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "ExtBloq";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.TextBox nametextBox;
+        private System.Windows.Forms.TextBox addresstextBox;
+        private System.Windows.Forms.TextBox phonetextBox;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Tesseract;
+using System.Web.UI;
+using Emgu.CV;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Structure;
+using Emgu.CV.Util;
+
+
+
+
+
+
+
+namespace ExtBloq
+{
+    public partial class Form1 : Form
+    {
+        private Bitmap originalImage;
+        private Rectangle nameROI = new Rectangle(365, 512, 1277, 58); // Example ROI coordinates for name field
+        private Rectangle addressROI = new Rectangle(365, 783, 1277, 60); // Example ROI coordinates for address field
+        private Rectangle phoneROI = new Rectangle(50, 190, 150, 50); // Example ROI coordinates for phone field
+
+
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Image Files (*.jpg;*.jpeg;*.png;*.bmp)|*.jpg;*.jpeg;*.png;*.bmp";
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                originalImage = new Bitmap(openFileDialog.FileName);
+                pictureBox1.Image = originalImage;
+
+                // Extract data from ROIs
+                string name = ExtractTextFromROI(nameROI);
+                string address = ExtractTextFromROI(addressROI);
+                string phone = ExtractTextFromROI(phoneROI);
+
+                // Display extracted data
+                nametextBox.Text = name;
+                addresstextBox.Text = address;
+                phonetextBox.Text = phone;
+            }
+
+        }
+
+        private string ExtractTextFromROI(Rectangle roi)
+        {
+            using (Bitmap roiImage = new Bitmap(roi.Width, roi.Height))
+            {
+                using (Graphics g = Graphics.FromImage(roiImage))
+                {
+                    g.DrawImage(originalImage, 0, 0, roi, GraphicsUnit.Pixel);
+                }
+
+                using (TesseractEngine engine = new TesseractEngine("C:\\Users\\ajaik\\Desktop\\Tesseract", "eng", EngineMode.Default))
+                {
+                    using (Tesseract.Page page = engine.Process(roiImage))
+                    {
+                        return page.GetText();
+
+                    }
+
+
+                    }
+                }
+            }
+
+        private void addresstextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+    }
+
+                
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
