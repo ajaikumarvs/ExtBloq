81c7abc9d4db01bdbe6a6ae0c051c1e587e1954c
ajai <ajaikumarvs@gmail.com>
2024-05-05 14:09:07 +0530

    added several new features for document extraction

---
+++ scan.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace ExtBloq
+{
+    partial class scan
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(scan));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.buttonScan = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.buttonPreview = new System.Windows.Forms.Button();
+            this.buttonRefreshList = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 53);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(522, 429);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // buttonScan
+            // 
+            this.buttonScan.Location = new System.Drawing.Point(540, 53);
+            this.buttonScan.Name = "buttonScan";
+            this.buttonScan.Size = new System.Drawing.Size(113, 43);
+            this.buttonScan.TabIndex = 1;
+            this.buttonScan.Text = "Quick Scan";
+            this.buttonScan.UseVisualStyleBackColor = true;
+            this.buttonScan.Click += new System.EventHandler(this.buttonScan_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(809, 115);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(157, 134);
+            this.listBox1.TabIndex = 2;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 37);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(522, 10);
+            this.progressBar1.TabIndex = 3;
+            // 
+            // buttonPreview
+            // 
+            this.buttonPreview.Location = new System.Drawing.Point(540, 102);
+            this.buttonPreview.Name = "buttonPreview";
+            this.buttonPreview.Size = new System.Drawing.Size(113, 43);
+            this.buttonPreview.TabIndex = 4;
+            this.buttonPreview.Text = "Additional Options";
+            this.buttonPreview.UseVisualStyleBackColor = true;
+            this.buttonPreview.Click += new System.EventHandler(this.buttonPreview_Click);
+            // 
+            // buttonRefreshList
+            // 
+            this.buttonRefreshList.Location = new System.Drawing.Point(857, 255);
+            this.buttonRefreshList.Name = "buttonRefreshList";
+            this.buttonRefreshList.Size = new System.Drawing.Size(109, 32);
+            this.buttonRefreshList.TabIndex = 5;
+            this.buttonRefreshList.Text = "Refresh Devices";
+            this.buttonRefreshList.UseVisualStyleBackColor = true;
+            this.buttonRefreshList.Click += new System.EventHandler(this.buttonRefreshList_Click);
+            // 
+            // scan
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(978, 524);
+            this.Controls.Add(this.buttonRefreshList);
+            this.Controls.Add(this.buttonPreview);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.buttonScan);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "scan";
+            this.Text = "Scan for documents";
+            this.Load += new System.EventHandler(this.scan_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button buttonScan;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button buttonPreview;
+        private System.Windows.Forms.Button buttonRefreshList;
+    }
+}
---
+++ scan.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WIA;
+
+using System.Drawing.Imaging;
+
+namespace ExtBloq
+{
+    public partial class scan : Form
+    {
+        private DeviceManager deviceManager;
+
+       
+        public scan()
+        {
+            InitializeComponent();
+            deviceManager = new DeviceManager();
+        }
+
+
+        private void scan_Load(object sender, EventArgs e)
+        {
+            /*
+            // Populate listbox with available scanners
+            foreach (DeviceInfo scanner in deviceManager.DeviceInfos)
+            {
+                if (scanner.Type == WiaDeviceType.ScannerDeviceType)
+                {
+                    listBox1.Items.Add(scanner.Properties["Name"].get_Value());
+                }
+            }
+            */
+        }
+        private bool IsScannerReady(DeviceInfo scannerInfo)
+        {
+            try
+            {
+                // Try connecting to the scanner to check its readiness
+                Device scanner = scannerInfo.Connect();
+                return true;
+            }
+            catch (COMException ex)
+            {
+                // If an exception is thrown, the scanner is not ready
+                return false;
+            }
+        }
+        private void buttonPreview_Click(object sender, EventArgs e)
+        {
+            // Check if a scanner is selected
+            if (listBox1.SelectedItem == null)
+            {
+                MessageBox.Show("Please select a scanner.");
+                return;
+            }
+
+            // Get selected scanner
+            string selectedScannerName = listBox1.SelectedItem.ToString();
+            Device scanner = null;
+            foreach (DeviceInfo scannerInfo in deviceManager.DeviceInfos)
+            {
+                if (scannerInfo.Type == WiaDeviceType.ScannerDeviceType &&
+                    scannerInfo.Properties["Name"].get_Value().ToString() == selectedScannerName)
+                {
+                    scanner = scannerInfo.Connect();
+                    break;
+                }
+            }
+
+            // Check if scanner is found
+            if (scanner == null)
+            {
+                MessageBox.Show("Scanner not found.");
+                return;
+            }
+
+            // Preview the document
+            try
+            {
+                // Use a standard dialog for image acquisition
+                WIA.CommonDialog commonDialog = new WIA.CommonDialog();
+                ImageFile image = (ImageFile)commonDialog.ShowAcquireImage(
+                    WiaDeviceType.ScannerDeviceType,
+                    WiaImageIntent.ColorIntent,
+                    WiaImageBias.MaximizeQuality,
+                    FormatID.wiaFormatJPEG,
+                    false,
+                    true,
+                    false);
+
+                // Check if the acquired image is not null
+                if (image != null)
+                {
+                    // Convert image data to byte array
+                    byte[] imageData = (byte[])image.FileData.get_BinaryData();
+
+                    // Display previewed image in PictureBox
+                    using (MemoryStream memoryStream = new MemoryStream(imageData))
+                    {
+                        pictureBox1.Image = Image.FromStream(memoryStream);
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Preview canceled or failed.");
+                }
+            }
+            catch (COMException ex)
+            {
+                MessageBox.Show($"An error occurred during the preview process: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+       private void buttonScan_Click(object sender, EventArgs e)
+        {// Check if a scanner is selected
+            if (listBox1.SelectedItem == null)
+            {
+                MessageBox.Show("Please select a scanner.");
+                return;
+            }
+
+            // Get selected scanner
+            string selectedScannerName = listBox1.SelectedItem.ToString();
+            Device scanner = null;
+            foreach (DeviceInfo scannerInfo in deviceManager.DeviceInfos)
+            {
+                if (scannerInfo.Type == WiaDeviceType.ScannerDeviceType &&
+                    scannerInfo.Properties["Name"].get_Value().ToString() == selectedScannerName)
+                {
+                    scanner = scannerInfo.Connect();
+                    break;
+                }
+            }
+
+            // Check if scanner is found
+            if (scanner == null)
+            {
+                MessageBox.Show("Scanner not found.");
+                return;
+            }
+
+            // Scan document
+            Item scannerItem = scanner.Items[1];
+            
+            ImageFile scannedImage = (ImageFile)scannerItem.Transfer(WIA.FormatID.wiaFormatJPEG);
+
+
+
+            // Retrieve image data
+            byte[] imageData = (byte[])scannedImage.FileData.get_BinaryData();
+
+            // Create a MemoryStream from the image data
+            using (MemoryStream memoryStream = new MemoryStream(imageData))
+            {
+                // Display scanned image in picturebox
+                pictureBox1.Image = Image.FromStream(memoryStream);
+            }
+
+            MessageBox.Show("Scan Complete!");
+        }
+
+        private void buttonRefreshList_Click(object sender, EventArgs e)
+        {
+            // Clear existing items in the listbox
+            listBox1.Items.Clear();
+
+            // Populate listbox with available scanners
+            foreach (DeviceInfo scanner in deviceManager.DeviceInfos)
+            {
+                if (scanner.Type == WiaDeviceType.ScannerDeviceType)
+                {
+                    listBox1.Items.Add(scanner.Properties["Name"].get_Value());
+                }
+            }
+        }
+    }
+    }
+    
+
+
